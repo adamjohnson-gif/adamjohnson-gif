bfadfedec993da89100187c01309f6436ae988e1
adamjohnson-gif <llamaguy82@gmail.com>
2026-09-11 10:23:33 +0700

    rf (removefile): Delete src/main.rs

--- src/main.rs
+++
@@ -1,8 +0,0 @@
-use std::io::{self, Write}; // 1. Import the Write trait
-
-fn main() {
-    print!("Hello, World!"); // 2. Print "Hello, World!" like a Rust beginner example
-    
-    // 3. Flush stdout so the text displays immediately
-    io::stdout().flush().unwrap(); 
-}
